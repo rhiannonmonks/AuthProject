d5e2555c892e1cd9c717030575bcd5217dac1921
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2023-01-01 16:56:14 +0000

    authentication error handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
@@ -9,7 +10,15 @@ function LoginScreen() {
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not log you in, please check your credentials or try again later"
+      );
+    }
+
     setIsAuthenticating(false);
   }
 
