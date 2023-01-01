63dc09c72bf0bd5ba4903590f3754dacb170bd0b
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2023-01-01 16:38:57 +0000

    creating a new user in Firebase

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyAsJegV_gp855DPKeF4jwM3tm0voG3eMhM";
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
